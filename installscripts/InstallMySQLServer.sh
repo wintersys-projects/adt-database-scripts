6ab496bc79d675b446570eb7e848c2742cd54ec9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-14 22:01:10 +0000

    Update InstallMySQLServer.sh

--- installscripts/InstallMySQLServer.sh
+++ installscripts/InstallMySQLServer.sh
@@ -36,10 +36,10 @@ fi
 BUILDOSVERSION="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOSVERSION'`"
 
 apt=""
-if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
     apt="/usr/bin/apt-get"
-elif ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
 then
     apt="/usr/sbin/apt-fast"
 fi
