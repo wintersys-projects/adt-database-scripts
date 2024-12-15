8372e648fddcc517cc595553a12bcacf12ee1ed8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 19:17:32 +0000

    Update BackupDatabase.sh

--- providerscripts/database/BackupDatabase.sh
+++ providerscripts/database/BackupDatabase.sh
@@ -42,9 +42,13 @@ WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WE
 WEBSITE_NAME="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $2}'`"
 
 
-DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
-DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
-DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+#DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+#DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+#DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
 
 if ( [ "${websiteDB}" = "" ] )
 then
