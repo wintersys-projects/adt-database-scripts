54f2c2ecb554e410c548142a22980665384f1a35
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 20:30:19 +0000

    Update BackupDatabase.sh

--- providerscripts/database/BackupDatabase.sh
+++ providerscripts/database/BackupDatabase.sh
@@ -46,9 +46,9 @@ WEBSITE_NAME="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $2}'`"
 #DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
 #DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
 
 if ( [ "${websiteDB}" = "" ] )
 then
