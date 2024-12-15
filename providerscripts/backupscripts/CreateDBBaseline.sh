12b8c4244fb43548c264a6e5b5c67c6761765a65
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 20:29:56 +0000

    Update CreateDBBaseline.sh

--- providerscripts/backupscripts/CreateDBBaseline.sh
+++ providerscripts/backupscripts/CreateDBBaseline.sh
@@ -81,9 +81,9 @@ websiteDB="${HOME}/backups/${WEBSITE_NAME}-DB-backup".tar.gz
 #DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
 #DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
 
 cd ${HOME}/backups
 
