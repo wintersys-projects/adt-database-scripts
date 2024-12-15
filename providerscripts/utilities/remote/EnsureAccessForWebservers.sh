0cd53d15fc172feefb914dc5100ac0419223aa5c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 19:19:35 +0000

    Update EnsureAccessForWebservers.sh

--- providerscripts/utilities/remote/EnsureAccessForWebservers.sh
+++ providerscripts/utilities/remote/EnsureAccessForWebservers.sh
@@ -33,9 +33,13 @@ else
         mysql="/usr/bin/mysql"
 fi
 
-DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
-DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
-DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+#DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+#DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+#DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
 
 if ( [ "${DB_N}" = "" ] || [ "${DB_P}" = "" ] || [ "${DB_U}" = "" ] )
 then
