4452da6013f126d5d1c2506b113fbbde95b9651a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 10:45:37 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -6,33 +6,33 @@ fi
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallGo.sh"
-${HOME}/installscripts/InstallGo.sh ${BUILDOS}
+${HOME}/installscripts/InstallGo.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallSoftwareProperties.sh"
-${HOME}/installscripts/InstallSoftwareProperties.sh ${BUILDOS}
+${HOME}/installscripts/InstallSoftwareProperties.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallCurl.sh"
-${HOME}/installscripts/InstallCurl.sh ${BUILDOS}
+${HOME}/installscripts/InstallCurl.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallLibSocketSSL.sh"
-${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
+${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
-${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS}
+${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS}
+${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallSendEmail.sh"
-${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS}
+${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallSysStat.sh"
-${HOME}/installscripts/InstallSysStat.sh ${BUILDOS}
+${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallRsync.sh"
-${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
+${HOME}/installscripts/InstallRsync.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
-${HOME}/installscripts/InstallDatabaseClient.sh ${BUILDOS}
+${HOME}/installscripts/InstallDatabaseClient.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallDatabaseServer.sh"
-${HOME}/installscripts/InstallDatabaseServer.sh ${BUILDOS}
+${HOME}/installscripts/InstallDatabaseServer.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
+${HOME}/installscripts/InstallJQ.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallCron.sh"
-${HOME}/installscripts/InstallCron.sh ${BUILDOS}
+${HOME}/installscripts/InstallCron.sh ${BUILDOS} &
 
 
 ${HOME}/installscripts/InstallMonitoringGear.sh
