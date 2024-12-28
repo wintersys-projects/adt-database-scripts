746ebbac09fdbf801f0a7a6f81c963382bee9f57
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 10:54:50 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -7,32 +7,51 @@ fi
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallSoftwareProperties.sh"
 ${HOME}/installscripts/InstallSoftwareProperties.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallCurl.sh"
 ${HOME}/installscripts/InstallCurl.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallLibSocketSSL.sh"
 ${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
 ${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallNetworkManager.sh"
 ${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallSendEmail.sh"
 ${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallSysStat.sh"
 ${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallFirewall.sh"
 ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallRsync.sh"
 ${HOME}/installscripts/InstallRsync.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
 ${HOME}/installscripts/InstallDatabaseClient.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallDatabaseServer.sh"
 ${HOME}/installscripts/InstallDatabaseServer.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallJQ.sh"
 ${HOME}/installscripts/InstallJQ.sh ${BUILDOS} &
+pids="${pids} $!"
 >&2 /bin/echo "${0} InstallCron.sh"
 ${HOME}/installscripts/InstallCron.sh ${BUILDOS} &
+pids="${pids} $!"
+
+for pid in ${pids}
+do
+        wait ${pid}
+done
 
 
 ${HOME}/installscripts/InstallMonitoringGear.sh
