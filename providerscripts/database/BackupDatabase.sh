1d5d387f6551c7314963165af9c3d621b6fed2eb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-23 19:05:49 +0000

    Update BackupDatabase.sh

--- providerscripts/database/BackupDatabase.sh
+++ providerscripts/database/BackupDatabase.sh
@@ -46,9 +46,13 @@ WEBSITE_NAME="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $2}'`"
 #DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
 #DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
+#DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+#DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+#DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
+
+DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSUSERNAME'`"
+DB_P="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSPASSWORD'`"
+DB_N="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSNAME'`"
 
 if ( [ "${websiteDB}" = "" ] )
 then
