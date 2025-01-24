01eb5d827a2c16c532acba46ed66af1ffc146963
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-24 08:54:46 +0000

    Update CreateDBBaseline.sh

--- providerscripts/backupscripts/CreateDBBaseline.sh
+++ providerscripts/backupscripts/CreateDBBaseline.sh
@@ -85,9 +85,9 @@ websiteDB="${HOME}/backups/${WEBSITE_NAME}-DB-backup".tar.gz
 #DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
 #DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
 
-DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSUSERNAME'`"
-DB_P="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSPASSWORD'`"
-DB_N="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSDBNAME'`"
+DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
+DB_P="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBPASSWORD'`"
+DB_N="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
 
 cd ${HOME}/backups
 
