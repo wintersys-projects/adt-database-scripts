be1c79efda3efdb86c50c5dc55105248dfffa98f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-29 12:13:35 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -22,6 +22,12 @@ ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallDatastoreTools.sh"
 ${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
 
+if ( [ ! -f /usr/bin/s3cmd ] && [ ! -f /usr/bin/s5cmd ] )
+then
+>&2 /bin/echo "${0} Failed to install essential datastore tooling, I have to exit"
+  exit
+fi
+
 >&2 /bin/echo "${0} InstallDatabaseServer.sh"
 ${HOME}/installscripts/InstallDatabaseServer.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
