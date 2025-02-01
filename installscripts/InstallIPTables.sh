d56fd0682de6a9f17d48e4b34e8db29c4d96de1c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-01 15:57:08 +0000

    Update InstallIPTables.sh

--- installscripts/InstallIPTables.sh
+++ installscripts/InstallIPTables.sh
@@ -22,11 +22,18 @@
 
 HOME="`/bin/cat /home/homedir.dat`"
 
-#if ( [ "${1}" != "" ] )
-#then
-#	buildos="${1}"
-#fi
-BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+if ( [ "${1}" != "" ] )
+then
+    buildos="${1}"
+fi
+
+if ( [ "${buildos}" = "" ] )
+then
+    BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+else 
+    BUILDOS="${buildos}"
+fi
+
 apt=""
 if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
