547158343aa0339f25ed9c8c04eed09d01635441
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-13 22:28:48 +0000

    Update InstallMySQLServer.sh

--- installscripts/InstallMySQLServer.sh
+++ installscripts/InstallMySQLServer.sh
@@ -53,31 +53,24 @@ if ( [ "${apt}" != "" ] )
 then
     if ( [ "${BUILDOS}" = "ubuntu" ] )
 	then
-             if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "MYSQL" | /usr/bin/awk -F':' '{print $NF}'`" != "cloud-init" ] )
-            then
         	eval ${install_command} gnupg   
  		mysql_apt_config="`/usr/bin/wget -O- -q https://dev.mysql.com/downloads/repo/apt/ | grep mysql-apt-config | grep -o '([^)]*)' | /bin/sed -e 's/(//' -e 's/)//'`"	
 		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} && /usr/bin/dpkg -i ${mysql_apt_config}	
 		/bin/rm ${mysql_apt_config}									
         	eval ${update_command} --allow-change-held-packages
 		eval ${install_command} mysql-server
-  	   fi
-
               /bin/mkdir /var/log/mysql
             /bin/chown mysql:mysql /var/log/mysql  
 	fi
 
 	if ( [ "${BUILDOS}" = "debian" ] )
 	then
-            if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "MYSQL" | /usr/bin/awk -F':' '{print $NF}'`" != "cloud-init" ] )
-            then
         	eval ${install_command} gnupg  		
 	 	mysql_apt_config="`/usr/bin/wget -O- -q https://dev.mysql.com/downloads/repo/apt/ | grep mysql-apt-config | grep -o '([^)]*)' | /bin/sed -e 's/(//' -e 's/)//'`"	
 		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} && /usr/bin/dpkg -i ${mysql_apt_config} 
 		/bin/rm ${mysql_apt_config}									
         	eval ${update_command} --allow-change-held-packages
 		eval ${install_command} mysql-server
-            fi
 
               /bin/mkdir /var/log/mysql
             /bin/chown mysql:mysql /var/log/mysql  
