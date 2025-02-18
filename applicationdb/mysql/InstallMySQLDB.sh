a49f17f26a144d5b02d5f46995c0f702c9d3e467
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-18 11:19:31 +0000

    Update InstallMySQLDB.sh

--- applicationdb/mysql/InstallMySQLDB.sh
+++ applicationdb/mysql/InstallMySQLDB.sh
@@ -96,14 +96,6 @@ then
     exit
 fi
 
-#Make absolutely certain we are all on INNODB
-tables="`${HOME}/providerscripts/utilities/remote/ConnectToMySQLDB.sh 'show tables' | /usr/bin/tail -n +2`"
-
-for table in ${tables}
-do
-   # /usr/bin/mysql -A -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}" -e "ALTER TABLE ${table} ENGINE = INNODB;"
-   ${HOME}/providerscripts/utilities/remote/ConnectToMySQLDB.sh "ALTER TABLE ${table} ENGINE = INNODB;"
-done
 
 #if ( [ "`/usr/bin/mysql -A -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}" -e 'show tables' | /usr/bin/wc -l`" -gt "5" ] )
 if ( [ "`${HOME}/providerscripts/utilities/remote/ConnectToMySQLDB.sh 'show tables' | /bin/grep 'zzzz'`" != "" ] )
