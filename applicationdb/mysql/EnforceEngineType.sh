13124b934ad5f339de295dab817afa836367105e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-18 11:20:01 +0000

    Create EnforceEngineType.sh

---
+++ applicationdb/mysql/EnforceEngineType.sh
@@ -0,0 +1,8 @@
+#Make absolutely certain we are all on INNODB
+tables="`${HOME}/providerscripts/utilities/remote/ConnectToMySQLDB.sh 'show tables' | /usr/bin/tail -n +2`"
+
+for table in ${tables}
+do
+   # /usr/bin/mysql -A -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}" -e "ALTER TABLE ${table} ENGINE = INNODB;"
+   ${HOME}/providerscripts/utilities/remote/ConnectToMySQLDB.sh "ALTER TABLE ${table} ENGINE = INNODB;"
+done
