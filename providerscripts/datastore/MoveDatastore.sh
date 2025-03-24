8ee5f5857792c4e90ed9a747ce76835103365eea
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-24 11:01:22 +0000

    Update MoveDatastore.sh

--- providerscripts/datastore/MoveDatastore.sh
+++ providerscripts/datastore/MoveDatastore.sh
@@ -25,11 +25,11 @@ new_object="$2"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd "
+	datastore_tool="/usr/bin/s3cmd "
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
+	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+	datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
 ${datastore_tool} mv s3://${original_object} s3://${new_object}
