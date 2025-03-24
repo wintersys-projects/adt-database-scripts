7c9922a014bed4bbdeb7aee7428d356a83e0fe5d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-24 11:12:21 +0000

    Update GitClone.sh

--- providerscripts/git/GitClone.sh
+++ providerscripts/git/GitClone.sh
@@ -29,43 +29,38 @@ directory="${6}"
 
 if ( [ "${repository_provider}" = "bitbucket" ] )
 then
-    if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
-    then
-        /usr/bin/git clone https://bitbucket.org/${repository_ownername}/${repository_name}.git
-    else
-        if ( [ "${repository_password}" = "none" ] )
-        then
-            /usr/bin/git clone https://${repository_username}@bitbucket.org/${repository_ownername}/${repository_name}.git ${directory}
-        else
-            /usr/bin/git clone https://${repository_username}:${repository_password}@bitbucket.org/${repository_ownername}/${repository_name}.git ${directory}
-        fi
-    fi
+	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+	then
+		/usr/bin/git clone https://bitbucket.org/${repository_ownername}/${repository_name}.git
+	elif ( [ "${repository_password}" = "none" ] )
+	then
+		/usr/bin/git clone https://${repository_username}@bitbucket.org/${repository_ownername}/${repository_name}.git ${directory}
+	else
+		/usr/bin/git clone https://${repository_username}:${repository_password}@bitbucket.org/${repository_ownername}/${repository_name}.git ${directory}
+	fi
 fi
 if ( [ "${repository_provider}" = "github" ] )
 then
-    if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
-    then
-        /usr/bin/git clone https://github.com/${repository_ownername}/${repository_name}.git
-    else
-        if ( [ "${repository_password}" = "none" ] )
-        then
-            /usr/bin/git clone https://${repository_username}@github.com/${repository_ownername}/${repository_name}.git ${directory}
-        else
-            /usr/bin/git clone https://${repository_username}:${repository_password}@github.com/${repository_ownername}/${repository_name}.git ${directory}
-        fi
-    fi
+	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+	then
+		/usr/bin/git clone https://github.com/${repository_ownername}/${repository_name}.git
+    elif ( [ "${repository_password}" = "none" ] )
+	then
+		/usr/bin/git clone https://${repository_username}@github.com/${repository_ownername}/${repository_name}.git ${directory}
+	else
+		/usr/bin/git clone https://${repository_username}:${repository_password}@github.com/${repository_ownername}/${repository_name}.git ${directory}
+	fi
 fi
+
 if ( [ "${repository_provider}" = "gitlab" ] )
 then
-    if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
-    then
-        /usr/bin/git clone https://gitlab.com/${repository_ownername}/${repository_name}.git
-    else
-        if ( [ "${repository_password}" = "none" ] )
-        then
-            /usr/bin/git clone https://${repository_username}@gitlab.com/${repository_ownername}/${repository_name}.git ${directory}
-        else
-            /usr/bin/git clone https://${repository_username}:${repository_password}@gitlab.com/${repository_ownername}/${repository_name}.git ${directory}
-        fi
-    fi
+	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+	then
+		/usr/bin/git clone https://gitlab.com/${repository_ownername}/${repository_name}.git
+	elif ( [ "${repository_password}" = "none" ] )
+	then
+		/usr/bin/git clone https://${repository_username}@gitlab.com/${repository_ownername}/${repository_name}.git ${directory}
+	else
+		/usr/bin/git clone https://${repository_username}:${repository_password}@gitlab.com/${repository_ownername}/${repository_name}.git ${directory}
+	fi
 fi
