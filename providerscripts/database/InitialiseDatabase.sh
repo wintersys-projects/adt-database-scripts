c945f3c88c8087426d87fbab472168563925a4c1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-24 10:50:44 +0000

    Update InitialiseDatabase.sh

--- providerscripts/database/InitialiseDatabase.sh
+++ providerscripts/database/InitialiseDatabase.sh
@@ -19,52 +19,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #####################################################################################
 #####################################################################################
-set -x
-
-#if ( [ ! -d ${HOME}/credentials ] )
-#then
- #       /bin/mkdir ${HOME}/credentials
- #       /bin/chmod 700 ${HOME}/credentials
-#fi
-
-#if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
-#then
- #   DBaaS_DBNAME="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSDBNAME'`"
- #   DBaaS_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSPASSWORD'`"
- #   DBaaS_USERNAME="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSUSERNAME'`"##
-
- #   /bin/echo "${DBaaS_DBNAME}" >>  ${HOME}/credentials/shit
- #   /bin/echo "${DBaaS_PASSWORD}" >>  ${HOME}/credentials/shit
- #   /bin/echo "${DBaaS_USERNAME}" >>  ${HOME}/credentials/shit
-    
- #   ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/credentials/shit credentials/shit
-#else
- #   rnd="n`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-8 | /usr/bin/tr '[:upper:]' '[:lower:]'`n"
- #   rnd1="p`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-8 | /usr/bin/tr '[:upper:]' '[:lower:]'`p"
- #   rnd2="u`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-8 | /usr/bin/tr '[:upper:]' '[:lower:]'`u"
-
-  #  /bin/echo "${rnd}" > ${HOME}/credentials/shit
-  #  /bin/echo "${rnd1}" >> ${HOME}/credentials/shit
-  #  /bin/echo "${rnd2}" >> ${HOME}/credentials/shit
-
-  #  ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/credentials/shit credentials/shit
-#fi
-
-#if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "credentials/shit"`" = "1" ]  )
-#then
- #   DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
- #   DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
- #   DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
-#else
-#if ( [ -f ${HOME}/credentials/db_cred ] )
-#then
-#    DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
-#    DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
-#    DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
-#else
-#        /bin/echo "Database credentials could not be found in datastore"
-#fi
-#fi
+#set -x
 
 DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
 DB_P="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBPASSWORD'`"
@@ -72,22 +27,18 @@ DB_N="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ]  )
 then
- #   . ${HOME}/providerscripts/database/singledb/mariadb/InstallMariaDB.sh
-    . ${HOME}/providerscripts/database/singledb/mariadb/InitialiseMariaDB.sh
+	. ${HOME}/providerscripts/database/singledb/mariadb/InitialiseMariaDB.sh
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] )
 then
-  #  . ${HOME}/providerscripts/database/singledb/postgres/InstallPostgresDB.sh
-    . ${HOME}/providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
+	. ${HOME}/providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
 then
-#    . ${HOME}/providerscripts/database/singledb/mysql/InstallMySQLDB.sh
-    . ${HOME}/providerscripts/database/singledb/mysql/InitialiseMySQLDB.sh
+	. ${HOME}/providerscripts/database/singledb/mysql/InitialiseMySQLDB.sh
 fi
 
 ${HOME}/providerscripts/email/SendEmail.sh "A single node database has been started" "a single node database has been started and initialised" "INFO"
-
 /bin/touch ${HOME}/runtime/DB_INITIALISED
