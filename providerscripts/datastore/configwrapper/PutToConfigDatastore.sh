d6132ed661df3816f746eac459a050b39e1ed483
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-24 11:06:46 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
@@ -33,38 +33,38 @@ configbucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd put "
+	datastore_tool="/usr/bin/s3cmd put "
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
+	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+	datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
 fi
 
 if ( [ ! -f ${file_to_put} ] )
 then
-        path_to_file="`/bin/echo ${file_to_put} | sed 's:/[^/]*$::' | /bin/sed 's,^/,,'`"
-        file="`/bin/echo "${file_to_put}" | /bin/grep "/" | /usr/bin/awk -F'/' '{print $NF}'`"
+	path_to_file="`/bin/echo ${file_to_put} | sed 's:/[^/]*$::' | /bin/sed 's,^/,,'`"
+	file="`/bin/echo "${file_to_put}" | /bin/grep "/" | /usr/bin/awk -F'/' '{print $NF}'`"
 
-        if ( [ "`/bin/echo ${file_to_put} | /bin/grep "/"`" = "" ] )
-        then
-                file_to_put="/tmp/${file_to_put}"
-        else
-                file_to_put="/tmp/${path_to_file}/${file}"
-                dir="`/bin/echo ${file_to_put} | /bin/sed 's:/[^/]*$::'`"
-                if ( [ ! -d ${dir} ] )
-                then
-                        /bin/mv ${dir} ${dir}.$$
-                        /bin/mkdir -p "${dir}"
-                fi
-        fi
-        /bin/touch ${file_to_put}
+	if ( [ "`/bin/echo ${file_to_put} | /bin/grep "/"`" = "" ] )
+	then
+		file_to_put="/tmp/${file_to_put}"
+	else
+		file_to_put="/tmp/${path_to_file}/${file}"
+		dir="`/bin/echo ${file_to_put} | /bin/sed 's:/[^/]*$::'`"
+		if ( [ ! -d ${dir} ] )
+		then
+			/bin/mv ${dir} ${dir}.$$
+			/bin/mkdir -p "${dir}"
+		fi
+	fi
+	/bin/touch ${file_to_put}
 fi
         
 if ( [ "${place_to_put}" != "" ] )
 then
-        command="${datastore_tool} ${file_to_put} s3://${configbucket}/${place_to_put}"
+	command="${datastore_tool} ${file_to_put} s3://${configbucket}/${place_to_put}"
 else
-        command="${datastore_tool} ${file_to_put} s3://${configbucket}"
+	command="${datastore_tool} ${file_to_put} s3://${configbucket}"
 fi
 
 count="0"
