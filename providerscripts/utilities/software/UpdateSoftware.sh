b1046aff4dbf2c5bc475e8cf428687291d1374f5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-24 11:27:50 +0000

    Update UpdateSoftware.sh

--- providerscripts/utilities/software/UpdateSoftware.sh
+++ providerscripts/utilities/software/UpdateSoftware.sh
@@ -21,15 +21,15 @@
 #set -x
 
 BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
-
+ 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDOS:ubuntu`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDOS:debian`" = "1" ] )
 then
-    ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
+	${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 fi
 
 for script in `/usr/bin/find ${HOME}/runtime/installedsoftware/ -name "*.sh" -print | /usr/bin/awk -F'/' '{print $NF}'`
 do
-        /bin/sh ${HOME}/installscripts/${script} ${BUILDOS}
+	/bin/sh ${HOME}/installscripts/${script} ${BUILDOS}
 done
 
 ${HOME}/providerscripts/utilities/software/UpdateInfrastructure.sh
