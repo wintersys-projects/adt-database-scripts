ef89149c9a8edb3ad9db3f9c25a3c4e2344e7271
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 17:12:51 +0000

    Update RemoveApplicationBranding.sh

--- providerscripts/application/branding/drupal/RemoveApplicationBranding.sh
+++ providerscripts/application/branding/drupal/RemoveApplicationBranding.sh
@@ -24,6 +24,14 @@
 #######################################################################################################
 #######################################################################################################
 
+HOME="`/bin/cat /home/homedir.dat`"
+WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
+WEBSITE_DISPLAY_NAME_UPPER="`/bin/echo ${WEBSITE_DISPLAY_NAME}  | /bin/sed 's/_/ /g' | /usr/bin/tr '[:lower:]' '[:upper:]'`"
+WEBSITE_DISPLAY_NAME_LOWER="`/bin/echo ${WEBSITE_DISPLAY_NAME} | /bin/sed 's/_/ /g' | /usr/bin/tr '[:upper:]' '[:lower:]'`"
+WEBSITE_DISPLAY_NAME="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME' | /bin/sed 's/_/ /g'`"
+ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/cut -d'.' -f2-`"
+
 domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
       
 /bin/sed -i "s/${domainspecifier}/ApplicationDomainSpec/g" applicationDB.sql
