5265420c7a3b298b8a9ff806596da5137dbaa4ae
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-20 13:43:46 +0100

    Update ConnectToMySQLDB.sh

--- providerscripts/utilities/remote/ConnectToMySQLDB.sh
+++ providerscripts/utilities/remote/ConnectToMySQLDB.sh
@@ -21,7 +21,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################
 #######################################################################################
-#set -x
+set -x
 
 SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
 SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
