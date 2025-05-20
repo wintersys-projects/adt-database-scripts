fde213e814e2adaf8177168c9164ae55d4bbef6a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-20 21:18:23 +0100

    Update InitialiseMySQLDB.sh

--- providerscripts/database/dbaas/linode/mysql/InitialiseMySQLDB.sh
+++ providerscripts/database/dbaas/linode/mysql/InitialiseMySQLDB.sh
@@ -59,8 +59,8 @@ then
     /bin/mkdir -p ${HOME}/runtime/mysql-init
 fi
 
-/bin/cp ${HOME}/providerscripts/database/dbaas/mysql/live/mysql-user.sql ${HOME}/runtime/mysql-init/initialiseDB-user.sql
-/bin/cp ${HOME}/providerscripts/database/dbaas/mysql/live/mysql-db.sql ${HOME}/runtime/mysql-init/initialiseDB.sql
+/bin/cp ${HOME}/providerscripts/database/dbaas/linode/mysql/live/mysql-user.sql ${HOME}/runtime/mysql-init/initialiseDB-user.sql
+/bin/cp ${HOME}/providerscripts/database/dbaas/linode/mysql/live/mysql-db.sql ${HOME}/runtime/mysql-init/initialiseDB.sql
 /bin/sed -i "s/XXXXDB_UXXXX/${DB_U2}/g" ${HOME}/runtime/mysql-init/initialiseDB.sql
 /bin/sed -i "s/XXXXDB_NXXXX/${DB_N}/g" ${HOME}/runtime/mysql-init/initialiseDB.sql
 /bin/sed -i "s/XXXXHOSTXXXX/${HOST}/g" ${HOME}/runtime/mysql-init/initialiseDB.sql
