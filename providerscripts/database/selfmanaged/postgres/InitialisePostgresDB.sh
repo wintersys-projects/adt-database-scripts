215e887a62211150a02554f80d948fe82fa82486
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-21 15:37:24 +0100

    Update InitialisePostgresDB.sh

--- providerscripts/database/selfmanaged/postgres/InitialisePostgresDB.sh
+++ providerscripts/database/selfmanaged/postgres/InitialisePostgresDB.sh
@@ -21,7 +21,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ####################################################################################
 ####################################################################################
-#set -x
+set -x
 
 IP_MASK="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'IPMASK'`"
 IP_MASK="`/bin/echo ${IP_MASK} | /bin/sed 's/%/0/g'`"
