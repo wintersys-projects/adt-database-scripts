032d3157957e10a85d37d79acb27aa2885c7e3dc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-23 09:51:51 +0100

    Update InstallApplicationDB.sh

--- applicationdb/postgres/InstallApplicationDB.sh
+++ applicationdb/postgres/InstallApplicationDB.sh
@@ -37,20 +37,20 @@ else
 fi
 
 #Arrange for and perform the installation of our database dump file into our postgres server
-if ( [ -f ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql ] )
+if ( [ -f ${HOME}/backups/installDB/${WEBSITE_NAME}DB.psql ] )
 then    
-	/bin/sed -i "s/XXXXXXXXXX/${DB_U}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
+	/bin/sed -i "s/XXXXXXXXXX/${DB_U}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.psql
 	IP_MASK="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'IPMASK'`"
-	/bin/sed -i "s/YYYYYYYYYY/${IP_MASK}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
+	/bin/sed -i "s/YYYYYYYYYY/${IP_MASK}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.psql
 	olduser="`/bin/grep 'u........u' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql | /bin/sed 's/ /\n/g' | grep '^u........u$' | /usr/bin/head -1`"
-	/bin/sed -i "s/${olduser}/${DB_U}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
+	/bin/sed -i "s/${olduser}/${DB_U}/g" ${HOME}/backups/installDB/${WEBSITE_NAME}DB.psql
 	export PGPASSWORD="${DB_P}"
         
 	#if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
 	#then
 #		${HOME}/providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
 #	fi
-	${HOME}/providerscripts/utilities/remote/ConnectToPostgresDB.sh < ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
+	${HOME}/providerscripts/utilities/remote/ConnectToPostgresDB.sh < ${HOME}/backups/installDB/${WEBSITE_NAME}DB.psql
 fi
 
 #We can gain confidence that our database has installed correctly if our special marker table is there
