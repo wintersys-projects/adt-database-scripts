ae8ee350a4738c07ccca7fba3b13962fd62f0d42
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-03 00:51:32 +0100

    Update RemoveApplicationBranding.sh

--- application/branding/wordpress/RemoveApplicationBranding.sh
+++ application/branding/wordpress/RemoveApplicationBranding.sh
@@ -29,9 +29,9 @@ HOME="`/bin/cat /home/homedir.dat`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 DB_U="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
 ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/cut -d'.' -f2-`"
-WEBSITE_DISPLAY_NAME_UPPER="`/bin/echo ${WEBSITE_DISPLAY_NAME}  | /bin/sed 's/_/ /g' | /usr/bin/tr '[:lower:]' '[:upper:]'`"
-WEBSITE_DISPLAY_NAME_LOWER="`/bin/echo ${WEBSITE_DISPLAY_NAME} | /bin/sed 's/_/ /g' | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-WEBSITE_DISPLAY_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME' | /bin/sed 's/_/ /g'`"
+WEBSITE_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITENAME' | /bin/sed 's/_/ /g'`"
+WEBSITE_NAME_UPPER="`/bin/echo ${WEBSITE_NAME}  | /bin/sed 's/_/ /g' | /usr/bin/tr '[:lower:]' '[:upper:]'`"
+WEBSITE_NAME_LOWER="`/bin/echo ${WEBSITE_NAME} | /bin/sed 's/_/ /g' | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 IP_MASK="`${HOME}/utilities/config/ExtractConfigValue.sh 'IPMASK'`"
 
 domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
@@ -40,9 +40,9 @@ domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i 
 /bin/sed -i "s/${WEBSITE_URL}/www.applicationdomain.tld/g" applicationDB.sql
 /bin/sed -i "s/@${ROOT_DOMAIN}/@applicationdomain.tld/g" applicationDB.sql
 /bin/sed -i "s/${ROOT_DOMAIN}/applicationdomain.tld/g" applicationDB.sql
-/bin/sed -i "s/${WEBSITE_DISPLAY_NAME}/GreatApplication/g" applicationDB.sql
-/bin/sed -i "s/${WEBSITE_DISPLAY_NAME_UPPER}/GREATAPPLICATION/g" applicationDB.sql
-/bin/sed -i "s/${WEBSITE_DISPLAY_NAME_LOWER}-online/application-online/g" applicationDB.sql
+/bin/sed -i "s/${WEBSITE_NAME}/GreatApplication/g" applicationDB.sql
+/bin/sed -i "s/${WEBSITE_NAME_UPPER}/GREATAPPLICATION/g" applicationDB.sql
+/bin/sed -i "s/${WEBSITE_NAME_LOWER}-online/application-online/g" applicationDB.sql
 /bin/sed -i "s/${DB_U}/XXXXXXXXXX/g" applicationDB.sql
 /bin/sed -i "s/@@mail/@mail/g" applicationDB.sql
 /bin/sed -i "s/${IP_MASK}/YYYYYYYYYY/g" applicationDB.sql
