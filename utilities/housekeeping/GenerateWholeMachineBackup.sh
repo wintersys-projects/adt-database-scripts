05bc50970bc984c3b5471621e2e74fbc01ebb9b4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-08 23:12:34 +0100

    Update GenerateWholeMachineBackup.sh

--- utilities/housekeeping/GenerateWholeMachineBackup.sh
+++ utilities/housekeeping/GenerateWholeMachineBackup.sh
@@ -12,8 +12,8 @@ while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
 do
   count="`/usr/bin/expr ${count} + 1`"
   cd ${HOME}/runtime  
-  /usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_runtime.tar.gz . 
-  /usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_runtime.tar . 
+  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
 done
 
 if ( [ "${count}" = "5" ] )
