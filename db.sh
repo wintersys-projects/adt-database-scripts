a374428b7a9f99fc98c0d4e475d30edfd5331d6e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-08 10:12:10 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -160,7 +160,19 @@ then
    		archive_name="database"
 	fi
 
-	/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / &
+  	count="1"
+	/bin/ls /tmp/dusty.$$
+ 
+	while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
+ 	do	
+  		count="`/usr/bin/expr ${count} + 1`"
+		/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / &
+	done
+
+ 	if ( [ "${count}" = "5" ] )
+  	then
+		${HOME}/providerscripts/email/SendEmail.sh "FAILED TO GENERATE WHOLE MACHINE BACKUP on the webserver" "It hasn't been possible to generate a whole machine backup on the webserverr machine" "ERROR"
+   	fi
 
 	DB_U="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
 	DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
