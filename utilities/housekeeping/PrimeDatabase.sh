8c4cac75e215aaebaf92f204d83f9f7237d92df4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-11 18:52:50 +0100

    Update PrimeDatabase.sh

--- utilities/housekeeping/PrimeDatabase.sh
+++ utilities/housekeeping/PrimeDatabase.sh
@@ -1,4 +1,5 @@
 HOME="`/bin/cat /home/homedir.dat`"
+BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
@@ -7,3 +8,6 @@ USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
 original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
 
 /bin/rm -r /home/${original_user}
+
+BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
