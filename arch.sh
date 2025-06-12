3a49cd07d37c2a2697c573564aa017d3d0500a56
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 21:19:38 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -21,60 +21,13 @@
 #######################################################################################################
 #set -x
 
-#Get ourselves orientated so that we know where our home is
-USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
-/bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
-/bin/chmod 644 /home/${USER_HOME}/.bashrc
-/bin/chown ${USER_HOME}:root /home/${USER_HOME}/.bashrc
 
-SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
-
-/bin/echo "set mouse=r" > /root/.vimrc
-
-#Set the intialial permissions for the build
-/usr/bin/find ${HOME} -not -path '*/\.*' -type d -print0 | xargs -0 chmod 0755 # for directories
-/usr/bin/find ${HOME} -not -path '*/\.*' -type f -print0 | xargs -0 chmod 0500 # for files
-/bin/chown ${SERVER_USER}:root ${HOME}/.ssh
-/bin/chmod 750 ${HOME}/.ssh
-
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/sh ${1} ${2} ${3} ${4} ${5} ${6}' > /usr/bin/run
-/bin/chown ${SERVER_USER}:root /usr/bin/run
-/bin/chmod 750 /usr/bin/run
-
-if ( [ ! -d ${HOME}/logs/initialbuild ] )
-then
-	/bin/mkdir -p ${HOME}/logs/initialbuild
-fi
-
-if ( [ ! -d ${HOME}/super ] )
-then
-	/bin/mkdir ${HOME}/super
-fi
- 
-if ( [ ! -d ${HOME}/.ssh ] )
-then
-	/bin/mkdir ${HOME}/.ssh
-	/bin/chmod 700 ${HOME}/.ssh
-fi
-
-if ( [ ! -d ${HOME}/runtime ] )
-then
-	/bin/mkdir -p ${HOME}/runtime
-	/bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/runtime
-	/bin/chmod 755 ${HOME}/runtime
-fi
-
-/bin/mv ${HOME}/utilities/security/Super.sh ${HOME}/super
-/bin/chmod 400 ${HOME}/super/Super.sh
+/bin/rm ${HOME}/logs/*
 
 out_file="initialbuild/database-build-out-`/bin/date | /bin/sed 's/ //g'`"
 exec 1>>${HOME}/logs/${out_file}
 err_file="initialbuild/database-build-err-`/bin/date | /bin/sed 's/ //g'`"
 exec 2>>${HOME}/logs/${err_file}
-
-/bin/echo "${0} Initialising Datastore"
-#${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
-#${HOME}/providerscripts/datastore/InitialiseAdditionalDatastoreConfigs.sh
 
 MYSQL_USER="mysql"
 MYSQL_PASSWORD="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
@@ -95,19 +48,6 @@ if ( [ -f ${HOME}/runtime/DB_APPLICATION_INSTALLED ] )
 then
 	/bin/rm ${HOME}/runtime/DB_APPLICATION_INSTALLED
 fi
-
-if ( [ "${BYPASS_DB_LAYER}" != "1" ] )
-then
-	#...and install the application
-	if ( [ "${BASELINE_DB_REPOSITORY_NAME}" != "VIRGIN" ] )
-	then    
-		/bin/echo "${0} Installing bespoke application"
-		${HOME}/application/db/InstallApplicationDB.sh 
-	fi
-fi
-
-/bin/echo "${0} Initialising crontab"
-${HOME}/cron/InitialiseCron.sh
 
 
 
