9e8c2052453d50a8320503396595e297d1284b13
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 20:39:19 +0100

    Update ResetClonedDatabase.sh

--- utilities/housekeeping/ResetClonedDatabase.sh
+++ utilities/housekeeping/ResetClonedDatabase.sh
@@ -3,13 +3,12 @@ BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
-USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
+#USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
 
-original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
+#original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
 
-/usr/bin/rsync -avrP /home/${original_user}/* ${HOME}/ --exclude=".ssh" --include='.*' --ignore-existing 
+#/usr/bin/rsync -avrP /home/${original_user}/* ${HOME}/ --exclude=".ssh" --include='.*' --ignore-existing 
 
-/bin/rm -r /home/${original_user}
+#/bin/rm -r /home/${original_user}
 
-BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
