bf8c54fbd9d2619515aba0aa089ce11c671d9eca
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 08:52:49 +0100

    Update GenerateWholeMachineBackup.sh

--- utilities/housekeeping/GenerateWholeMachineBackup.sh
+++ utilities/housekeeping/GenerateWholeMachineBackup.sh
@@ -13,6 +13,7 @@ do
   count="`/usr/bin/expr ${count} + 1`"
   cd ${HOME}/runtime  
   /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_runtime.tar . 
+  /usr/bin/tar cpf ${HOME}/machinedump/${archive_name}_hidden.tar `/usr/bin/find / -type f -name '.*' -not -path "${HOME}/machinedump/*" -not -path "/var/www/html/*" -not -path '/dev/*' -not -path '/proc/*' -not -path  '/sys/*' -not -path '/tmp/*' -not -path '/run/*' -not -path '/mnt/*' -not -path '/media/*' -not -path '/lost+found/*'`
   /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${HOME}/machinedump/*" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
 done
 
