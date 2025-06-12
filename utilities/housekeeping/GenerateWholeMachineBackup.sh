9f96c0a4a7a49fa894a09b18b587a58aa5be0216
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 19:29:59 +0100

    Update GenerateWholeMachineBackup.sh

--- utilities/housekeeping/GenerateWholeMachineBackup.sh
+++ utilities/housekeeping/GenerateWholeMachineBackup.sh
@@ -5,16 +5,28 @@ fi
 
 archive_name="database"
 
+SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+SERVER_USER_PASSWORD="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
+
 count="1"
-/bin/ls /tmp/dusty.$$
- 
-while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
+success="0"
+
+while ( [ "${success}" = "0" ] && [ "${count}" -lt "5" ] )
 do
-  count="`/usr/bin/expr ${count} + 1`"
-  cd ${HOME}/runtime  
-  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_runtime.tar . 
+  success="1"
+  count="`/usr/bin/expr ${count} + 1`" 
+  /bin/echo "USERNAME:${SERVER_USER}" > ${HOME}/machinedump/credentials.dat
+  /bin/echo "PASSWORD:${SERVER_USER_PASSWORD}" >> ${HOME}/machinedump/credentials.dat
   /usr/bin/tar cpf ${HOME}/machinedump/${archive_name}_hidden.tar `/usr/bin/find / -type f -name '.*' -not -path "${HOME}/machinedump/*" -not -path "/var/www/html/*" -not -path '/dev/*' -not -path '/proc/*' -not -path  '/sys/*' -not -path '/tmp/*' -not -path '/run/*' -not -path '/mnt/*' -not -path '/media/*' -not -path '/lost+found/*'`
-  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${HOME}/machinedump/*" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+  if ( [ "$?" != "0" ] )
+  then
+    success="0"
+  fi
+  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${HOME}/machinedump/*" --exclude='/var/www/html/*' --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+  if ( [ "$?" != "0" ] )
+  then
+    success="0"
+  fi
 done
 
 if ( [ "${count}" = "5" ] )
