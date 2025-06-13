112b1b2ff06416a9562227461c30284e14e382e0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-13 09:33:31 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -19,7 +19,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+set -x
 
 USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
 /bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
