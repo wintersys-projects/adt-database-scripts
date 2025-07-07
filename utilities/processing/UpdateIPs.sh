6ef99d5bbbac474a2229e39ca4063a7302bd13ac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-07 23:54:49 +0100

    Update UpdateIPs.sh

--- utilities/processing/UpdateIPs.sh
+++ utilities/processing/UpdateIPs.sh
@@ -34,7 +34,7 @@ ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /tmp/${p
 if ( [ "${MULTI_REGION}" = "1" ] )
 then
         multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-        ${HOME}/providerscripts/datastore/PutToDatastore.sh ${public_ip} ${multi_region_bucket}/dbaas_ips/${public_ip} "no"
+        ${HOME}/providerscripts/datastore/PutToDatastore.sh ${publicip} ${multi_region_bucket}/dbaas_ips/${public_ip} "no"
 fi
 
 
