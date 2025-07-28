b6c4ef3cfa3a41196c501b3be6ff62958f2d34de
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-28 23:53:29 +0100

    Update GitClone.sh

--- providerscripts/git/GitClone.sh
+++ providerscripts/git/GitClone.sh
@@ -22,10 +22,10 @@
 
 repository_provider="${1}"
 repository_username="${2}"
-repository_password="${3}"
-repository_ownername="${4}"
-repository_name="${5}"
-directory="${6}"
+repository_ownername="${3}"
+repository_name="${4}"
+directory="${5}"
+repository_password="${6}"
 
 if ( [ "${repository_provider}" = "bitbucket" ] )
 then
