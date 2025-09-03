5a326a055c7f592527449c6772a6d2adb09c3f46
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-03 10:20:24 +0100

    Update InstallPackageManager.sh

--- installscripts/InstallPackageManager.sh
+++ installscripts/InstallPackageManager.sh
@@ -54,7 +54,7 @@ then
 	if ( [ "${BUILDOS}" = "ubuntu" ] )
 	then
 		/bin/bash -c "$(curl -sL https://git.io/vokNn)"
-		/usr/local/bin/apt-fast /usr/sbin/apt-fast
+		/usr/bin/ln -s /usr/local/bin/apt-fast /usr/sbin/apt-fast
 
 		if ( [ -f /etc/apt/sources.list.d/ubuntu.sources ] )
 		then
@@ -79,7 +79,7 @@ then
 	if ( [ "${BUILDOS}" = "debian" ] )
 	then
 		/bin/bash -c "$(curl -sL https://git.io/vokNn)"
-		/usr/local/bin/apt-fast /usr/sbin/apt-fast
+		/usr/bin/ln -s /usr/local/bin/apt-fast /usr/sbin/apt-fast
 
 		if ( [ -f /etc/apt/mirrors/debian.list ] )
 		then
