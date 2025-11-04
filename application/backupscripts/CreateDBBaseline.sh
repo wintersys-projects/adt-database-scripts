a2132da58290e4f0121c1d6f1c0262552b5be096
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-04 15:32:43 +0000

    Update CreateDBBaseline.sh

--- application/backupscripts/CreateDBBaseline.sh
+++ application/backupscripts/CreateDBBaseline.sh
@@ -25,42 +25,42 @@ DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
 
 if ( [ "${1}" = "" ] )
 then
-	/bin/echo "Your application type is set to: `${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATION'`"
-	/bin/echo "Please make very sure this is correct for your application otherwise things will break"
-	/bin/echo "Press <enter> when you are sure"
-	read x
+        /bin/echo "Your application type is set to: `${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATION'`"
+        /bin/echo "Please make very sure this is correct for your application otherwise things will break"
+        /bin/echo "Press <enter> when you are sure"
+        read x
 
-	/bin/echo "Please enter a unique identifier for your baseline and make sure you have created a repository with the name <identifier>-db-baseline with your repository provider"
-	read baseline_name
+        /bin/echo "Please enter a unique identifier for your baseline and make sure you have created a repository with the name <identifier>-db-baseline with your repository provider"
+        read baseline_name
 else
-	baseline_name="${1}"
+        baseline_name="${1}"
 fi
 
 if ( [ "${baseline_name}" = "" ] )
 then
-	/bin/echo "Identifier can't be blank"
-	exit
+        /bin/echo "Identifier can't be blank"
+        exit
 fi
 
 /bin/echo "Creating baseline of your database"
 
 if ( [ ! -d ${HOME}/logs/backups ] )
 then
-	/bin/mkdir -p ${HOME}/logs/backups
+        /bin/mkdir -p ${HOME}/logs/backups
 fi
 
 if ( [ "${1}" != "" ] )
 then
-	/bin/echo "with the following logs available on your database server"
-	#The log files for the server build are written here...
-	log_file="baseline_out_`/bin/date | /bin/sed 's/ //g'`"
-	err_file="baseline_err_`/bin/date | /bin/sed 's/ //g'`"
+        /bin/echo "with the following logs available on your database server"
+        #The log files for the server build are written here...
+        log_file="baseline_out_`/bin/date | /bin/sed 's/ //g'`"
+        err_file="baseline_err_`/bin/date | /bin/sed 's/ //g'`"
 
-	/bin/echo "Log file is at: ${HOME}/logs/backups/${log_file}"
-	/bin/echo "Error file is at: ${HOME}/logs/backups/${err_file}"
+        /bin/echo "Log file is at: ${HOME}/logs/backups/${log_file}"
+        /bin/echo "Error file is at: ${HOME}/logs/backups/${err_file}"
 
-	exec 1>>${HOME}/logs/backups/${log_file}
-	exec 2>>${HOME}/logs/backups/${err_file}
+        exec 1>>${HOME}/logs/backups/${log_file}
+        exec 2>>${HOME}/logs/backups/${err_file}
 fi
 
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
@@ -73,59 +73,59 @@ WEBSITE_DISPLAY_NAME_LOWER="`/bin/echo ${WEBSITE_DISPLAY_NAME} | /bin/sed 's/_/ 
 
 if ( [ ! -d ${HOME}/backups ] )
 then
-	/bin/mkdir -p ${HOME}/backups
+        /bin/mkdir -p ${HOME}/backups
 fi
 
 /bin/rm -r ${HOME}/backups/* 2>/dev/null
 if ( [ -d ${HOME}/.git ] )
 then
-	/bin/rm -r ${HOME}/.git
+        /bin/rm -r ${HOME}/.git
 fi
 
 APPLICATION_REPOSITORY_PROVIDER="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYPROVIDER'`"
 APPLICATION_REPOSITORY_USERNAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYUSERNAME'`"
-APPLICATION_REPOSITORY_PASSWORD="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYPASSWORD'`"
+APPLICATION_REPOSITORY_TOKEN="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYTOKEN'`"
 APPLICATION_REPOSITORY_OWNER="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYOWNER'`"
 #websiteDB="${HOME}/backups/${WEBSITE_NAME}-DB-backup".tar.gz
 
-if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PASSWORD} 2>&1 | /bin/grep 'Repository not found'`" != "" ] )
+if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_TOKEN} 2>&1 | /bin/grep 'Repository not found'`" != "" ] )
 then
-	if ( [ "${1}" = "" ] )
-	then
-		/bin/echo "Repository not found, do you want me to create one () (Y|y)"
-		read response
-		if ( [ "`/bin/echo "Y y" | /bin/grep ${response}`" != "" ] )
-		then
-			/bin/echo "Creating a new repository"
-			${HOME}/providerscripts/git/CreateRepository.sh ${APPLICATION_REPOSITORY_USERNAME} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_PASSWORD}
-			if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PASSWORD} 2>&1 | /bin/grep 'Repository not found'`" = "" ] )
-			then
-				/bin/echo "Repository (${baseline_name}-db-baseline) successfully created"
-				/bin/echo "Press <enter> to continue"
-				read x
-			else
-				/bin/echo "Repository (${baseline_name}-db-baseline) not created I will need to exit"
-				exit 1
-			fi
-		fi
-	else
-		${HOME}/providerscripts/git/CreateRepository.sh ${APPLICATION_REPOSITORY_USERNAME} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_PASSWORD}
-		if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PASSWORD} 2>&1 | /bin/grep 'Repository not found'`" = "" ] )
-		then
-			/bin/echo "Repository (${baseline_name}-db-baseline) successfully created"
-		else
-			/bin/echo "Repository (${baseline_name}-db-baseline) not created I will need to exit"
-			exit 1
-		fi
-	fi
-elif ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PASSWORD }2>&1`" = "" ] )
+        if ( [ "${1}" = "" ] )
+        then
+                /bin/echo "Repository not found, do you want me to create one () (Y|y)"
+                read response
+                if ( [ "`/bin/echo "Y y" | /bin/grep ${response}`" != "" ] )
+                then
+                        /bin/echo "Creating a new repository"
+                        ${HOME}/providerscripts/git/CreateRepository.sh ${APPLICATION_REPOSITORY_USERNAME} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_TOKEN}
+                        if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_TOKEN} 2>&1 | /bin/grep 'Repository not found'`" = "" ] )
+                        then
+                                /bin/echo "Repository (${baseline_name}-db-baseline) successfully created"
+                                /bin/echo "Press <enter> to continue"
+                                read x
+                        else
+                                /bin/echo "Repository (${baseline_name}-db-baseline) not created I will need to exit"
+                                exit 1
+                        fi
+                fi
+        else
+                ${HOME}/providerscripts/git/CreateRepository.sh ${APPLICATION_REPOSITORY_USERNAME} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_TOKEN}
+                if ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_TOKEN} 2>&1 | /bin/grep 'Repository not found'`" = "" ] )
+                then
+                        /bin/echo "Repository (${baseline_name}-db-baseline) successfully created"
+                else
+                        /bin/echo "Repository (${baseline_name}-db-baseline) not created I will need to exit"
+                        exit 1
+                fi
+        fi
+elif ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_TOKEN }2>&1`" = "" ] )
 then
-	/bin/echo "Suitable repo (${baseline_name}-db-baseline) found, press <enter> to continue"
-	read x
-elif ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_PASSWORD} 2>&1`" != "" ] )
+        /bin/echo "Suitable repo (${baseline_name}-db-baseline) found, press <enter> to continue"
+        read x
+elif ( [ "`${HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${baseline_name}-db-baseline ${APPLICATION_REPOSITORY_TOKEN} 2>&1`" != "" ] )
 then
-	/bin/echo "repository (${baseline_name}-db-baseline) found but its not empty. Please either empty the repository or delete it or rename it and allow this script to create a fresh one. Will exit now, please rerun me once this is actioned"
-	exit 1
+        /bin/echo "repository (${baseline_name}-db-baseline) found but its not empty. Please either empty the repository or delete it or rename it and allow this script to create a fresh one. Will exit now, please rerun me once this is actioned"
+        exit 1
 fi
 
 DB_U="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
@@ -150,7 +150,7 @@ ${HOME}/application/branding/RemoveApplicationBranding.sh
 
 if ( [ -f ./gitattibutes ] )
 then
-	/usr/bin/git add .gitattributes
+        /usr/bin/git add .gitattributes
 fi
 
 /usr/bin/git add .
@@ -162,13 +162,13 @@ REPOSITORY_PROVIDER="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIO
 #You can add additional repository providers here if you want to create a baseline with a different provider
 if ( [ "${REPOSITORY_PROVIDER}" = "bitbucket" ] )
 then
-	/usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_PASSWORD}@bitbucket.org/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
+        /usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_TOKEN}@bitbucket.org/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
 elif ( [ "${REPOSITORY_PROVIDER}" = "github" ] )
 then
-	/usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_PASSWORD}@github.com/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
+        /usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_TOKEN}@github.com/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
 elif ( [ "${REPOSITORY_PROVIDER}" = "gitlab" ] )
 then
-	/usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_PASSWORD}@gitlab.com/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
+        /usr/bin/git remote add origin https://${APPLICATION_REPOSITORY_USERNAME}:${APPLICATION_REPOSITORY_TOKEN}@gitlab.com/${APPLICATION_REPOSITORY_OWNER}/${baseline_name}-db-baseline.git
 fi
 
 /usr/bin/git push -u -f origin main
