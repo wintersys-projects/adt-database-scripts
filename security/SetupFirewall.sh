59cce1df09b51ae12bba265f013f412db21424b1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-11 14:33:52 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -24,6 +24,10 @@
 export HOME="`/bin/cat /home/homedir.dat`"
 BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
+if ( [ "`/usr/bin/find ${HOME}/runtime/customfirewallports.dat -mmin -1 -print`" != "" ] )
+then
+        /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
+fi
 
 if ( [ -f ${HOME}/runtime/FIREWALL-ACTIVE ] )
 then
@@ -53,31 +57,36 @@ then
 	firewall="iptables"
 fi
 
-if ( [ "${firewall}" = "ufw" ] && [ ! -f ${HOME}/runtime/FIREWALL-ACTIVE ] )
+if ( [ ! -f ${HOME}/runtime/FIREWALL-INITIALISED ] )
 then
-	/usr/bin/yes | /usr/sbin/ufw reset
-	/usr/sbin/ufw delete allow 22/tcp
-	/bin/sed -i "s/IPV6=yes/IPV6=no/g" /etc/default/ufw
-	/usr/sbin/ufw logging off
-	/usr/sbin/ufw reload
-elif ( [ "${firewall}" = "iptables" ] && [ ! -f ${HOME}/runtime/FIREWALL-ACTIVE ] )
-then
-	/usr/sbin/iptables -P INPUT DROP
-	/usr/sbin/iptables -P FORWARD DROP
-	/usr/sbin/iptables -P OUTPUT ACCEPT
-	/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
-	/usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+	if ( [ "${firewall}" = "ufw" ] )
+	then
+		/usr/bin/yes | /usr/sbin/ufw reset
+		/usr/sbin/ufw delete allow 22/tcp
+		/bin/sed -i "s/IPV6=yes/IPV6=no/g" /etc/default/ufw
+		/usr/sbin/ufw logging off
+		/usr/sbin/ufw reload
+		/bin/touch ${HOME}/runtime/FIREWALL-INITIALISED 
+	elif ( [ "${firewall}" = "iptables" ] )
+	then
+		/usr/sbin/iptables -P INPUT DROP
+		/usr/sbin/iptables -P FORWARD DROP
+		/usr/sbin/iptables -P OUTPUT ACCEPT
+		/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
+		/usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
 
-	/usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-	/usr/sbin/iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-	/usr/sbin/iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-	/usr/sbin/iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+		/usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+		/usr/sbin/iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+		/usr/sbin/iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+		/usr/sbin/iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
         
-	/usr/sbin/ip6tables -P INPUT DROP
-	/usr/sbin/ip6tables -P FORWARD DROP
-	/usr/sbin/ip6tables -P OUTPUT ACCEPT
-	/usr/sbin/ip6tables -A INPUT -i lo -j ACCEPT
-	/usr/sbin/ip6tables -A OUTPUT -o lo -j ACCEPT
+		/usr/sbin/ip6tables -P INPUT DROP
+		/usr/sbin/ip6tables -P FORWARD DROP
+		/usr/sbin/ip6tables -P OUTPUT ACCEPT
+		/usr/sbin/ip6tables -A INPUT -i lo -j ACCEPT
+		/usr/sbin/ip6tables -A OUTPUT -o lo -j ACCEPT
+		/bin/touch ${HOME}/runtime/FIREWALL-INITIALISED 
+	fi
 fi
 
 DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
@@ -169,26 +178,39 @@ custom_ports="`/bin/grep "^DATABASECUSTOMPORTS" ${HOME}/runtime/customfirewallpo
 
 for custom_port_token in ${custom_ports}
 do
+        delete="no"
         if ( [ "`/bin/echo ${custom_port_token} | /bin/grep 'ipv4'`" != "" ] )
         then
                 port="`/bin/echo ${custom_port_token} | /usr/bin/awk -F'|' '{print $1}'`"
                 ip_address="`/bin/echo ${custom_port_token} | /usr/bin/awk -F'|' '{print $3}'`"
+                if ( [ "`/bin/echo ${custom_port_token} | /usr/bin/awk -F'|' '{print $4}'`" = "DELETE" ] )
+                then
+                        delete="yes"
+                fi
         fi
 
         if ( [ "${firewall}" = "ufw" ] )
         then
-                if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${ip_address} | /bin/grep ALLOW`" = "" ] )
+                if ( [ "`/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw status | /bin/grep ${port} | /bin/grep ALLOW`" = "" ] )
                 then
                         /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow from ${ip_address} to any port ${port}
                         updated="1"
                 fi
         elif ( [ "${firewall}" = "iptables" ] )
         then
-                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${ip_address}`" = "" ] )
+                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${port}`" = "" ] && [ "${delete}" != "yes" ] )
                 then
                         /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
                         /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
                         updated="1"
+                else
+                        if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep ACCEPT | /bin/grep ${port}`" != "" ] && [ "${delete}" = "yes" ] )
+                        then
+                                /usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+                                /usr/sbin/iptables -D OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
+                                updated="1"
+                        fi
+
                 fi
         fi
 done
