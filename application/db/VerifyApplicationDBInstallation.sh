dd0166c6f2158241762432a2cc9839efdf54bb71
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-04 11:31:34 +0000

    Update VerifyApplicationDBInstallation.sh

--- application/db/VerifyApplicationDBInstallation.sh
+++ application/db/VerifyApplicationDBInstallation.sh
@@ -1,1 +1,17 @@
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] )
+then
+	${HOME}/application/branding/ApplyApplicationBranding.sh
+	${HOME}/application/db/maria/InstallApplicationDB.sh
 
+fi
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
+then
+	${HOME}/application/branding/ApplyApplicationBranding.sh
+	${HOME}/application/db/mysql/InstallApplicationDB.sh
+fi
+
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] )
+then
+	${HOME}/application/branding/ApplyApplicationBranding.sh
+	${HOME}/application/db/postgres/InstallApplicationDB.sh
+fi
