d90893b6575c5979a687c57c0dbdb5a7a2446964
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-04 11:34:18 +0000

    Update VerifyApplicationDBInstallation.sh

--- application/db/VerifyApplicationDBInstallation.sh
+++ application/db/VerifyApplicationDBInstallation.sh
@@ -1,17 +1,30 @@
+#!/bin/sh
+
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] )
 then
-	${HOME}/application/branding/ApplyApplicationBranding.sh
-	${HOME}/application/db/maria/InstallApplicationDB.sh
-
+	if ( [ "`${HOME}/application/db/maria/VerifyApplicationDBInstallation.sh`" = "1" ] )
+	then
+		/bin/echo "1"
+	else
+		/bin/echo "0"
+	fi
 fi
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] )
 then
-	${HOME}/application/branding/ApplyApplicationBranding.sh
-	${HOME}/application/db/mysql/InstallApplicationDB.sh
+	if ( [ "`${HOME}/application/db/mysql/VerifyApplicationDBInstallation.sh`" = "1" ] )
+	then
+		/bin/echo "1"
+	else
+		/bin/echo "0"
+	fi
 fi
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] )
 then
-	${HOME}/application/branding/ApplyApplicationBranding.sh
-	${HOME}/application/db/postgres/InstallApplicationDB.sh
+	if ( [ "`${HOME}/application/db/postgres/VerifyApplicationDBInstallation.sh`" = "1" ] )
+	then
+		/bin/echo "1"
+	else
+		/bin/echo "0"
+	fi
 fi
