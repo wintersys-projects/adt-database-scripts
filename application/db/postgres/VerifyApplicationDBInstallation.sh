73a36760fff19830dc0e3ff445b6109b7739326e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-04 11:29:03 +0000

    Update VerifyApplicationDBInstallation.sh

--- application/db/postgres/VerifyApplicationDBInstallation.sh
+++ application/db/postgres/VerifyApplicationDBInstallation.sh
@@ -1,1 +1,8 @@
-${HOME}/utilities/remote/ConnectToPostgresDB.sh "\dt" "raw" | /bin/grep "^zzzz$"
+#!/bin/sh
+
+if ( [ "`${HOME}/utilities/remote/ConnectToPostgresDB.sh "\dt" "raw" | /bin/grep "^zzzz$"`" != "" ] )
+then
+        /bin/echo "1" 
+else
+        /bin/echo "0" 
+fi
