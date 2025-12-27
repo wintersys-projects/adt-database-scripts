c42d9949c7083e340656cc6c820c6509fdd4ba98
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-27 00:32:13 +0000

    Update VirusScan.sh

--- utilities/security/VirusScan.sh
+++ utilities/security/VirusScan.sh
@@ -2,7 +2,8 @@
 ######################################################################################
 # Author : Peter Winter
 # Date   : 11/08/2021
-# Description: This script will scan your filesystem for viruses 
+# Description: This script will scan your filesystem for viruses. This can help protect
+# Windows users in some circumstances
 #######################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
@@ -32,7 +33,10 @@ then
   /bin/mkdir -p ${HOME}/runtime/virus_report
 fi
 
-/usr/bin/clamscan --max-filesize=2000M --max-scansize=2000M --recursive=yes --infected / > ${HOME}/runtime/virus_report/latest.log  2>/dev/null
+if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'VIRUSSCANNER:clamav'`" = "1" ] )
+then
+  /usr/bin/clamscan --max-filesize=2000M --max-scansize=2000M --recursive=yes --infected /var /home /tmp > ${HOME}/runtime/virus_report/latest.log  2>/dev/null
+fi
 
 if ( [ ! -f ${HOME}/runtime/virus_report/latest.log ] || [ "`/usr/bin/find ${HOME}/runtime/virus_report/latest.log -cmin -5`" = "" ] )
 then
