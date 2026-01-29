a10f3ff0a4a5e99e436b20fd53d5649480ef07ee
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-29 14:26:44 +0000

    Update InitialiseCron.sh

--- cron/InitialiseCron.sh
+++ cron/InitialiseCron.sh
@@ -55,7 +55,7 @@ HOME="`/bin/cat /home/homedir.dat`"
 /bin/echo "30 3 * * *  export HOME="${HOME}" && ${HOME}/utilities/housekeeping/RemoveExpiredLogs.sh" >> /var/spool/cron/crontabs/root
 
 
-/bin/echo '@hourly export HOME="'${HOME}'" && '${HOME}'/providerscripts/datastore/config/wrapper/DeleteFromDatastore.sh "config" "dbbackuplock.file" "local"' >> /var/spool/cron/crontabs/root
+/bin/echo '@hourly export HOME="'${HOME}'" && '${HOME}'/providerscripts/datastore/config/wrapper/DeleteFromDatastore.sh "config" "dbbackuplock.file"' >> /var/spool/cron/crontabs/root
 /bin/echo "@hourly export HOME="${HOME}" && ${HOME}/utilities/status/LoadMonitoring.sh" >> /var/spool/cron/crontabs/root
 
 SERVER_TIMEZONE_CONTINENT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERTIMEZONECONTINENT'`"
